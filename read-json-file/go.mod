0725ef9db3cda65a0a8a2d012aa28e1b988285d4
Victor Nascimento <victor.nascimento@bleu.com.br>
2021-10-26 11:43:47 -0300

    new stuff

---
+++ read-json-file/go.mod
@@ -0,0 +1,3 @@
+module read-json-file
+
+go 1.17
---
+++ read-json-file/main.go
@@ -0,0 +1,27 @@
+package read_json_file
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	jsonFile, err := os.Open("shakespeare.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var quotes []string
+
+	json.Unmarshal(byteValue, &quotes)
+
+	for i := 0; i < len(quotes); i++ {
+		println(quotes[i])
+	}
+}
